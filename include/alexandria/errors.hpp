0d19b6249940857dd9b6bf3aadbd2e3bf05520ec
brobeson <brobeson@users.noreply.github.com>
2026-06-26 13:37:09 +0800

    Update errors.hpp

--- include/alexandria/errors.hpp
+++ include/alexandria/errors.hpp
@@ -1,11 +1,23 @@
 #ifndef ALEXANDRIA_ERRORS_HPP
 #define ALEXANDRIA_ERRORS_HPP
 
+// iterating range: https://godbolt.org/z/4bGY9fn7n
+// errors example: https://godbolt.org/z/ssq6oGd1d
+// TODO Stringify the actual condition values?
+
 #include <stdexcept>
 
-#ifndef ALEX_ASSERT
-#define ALEX_ASSERT(condition) \
-  if (!(condition)) { \
-    throw std::runtime_error{#condition}; \
+namespace alex {
+  struct unmet_expectation: public std::logic_error {
+    explicit unmet_expectation(const char* const message):
+      std::logic_error(message) {}
+  };
+}
+
+#define ALEX_STRINGIFY_DETAIL(x) #x
+#define ALEX_STRINGIFY(x) ALEX_STRINGIFY_DETAIL(x)
+
+#define ALEX_EXPECTS( condition) \
+  if (!(condition)) [[unlikely]] {            \
+    throw alex::unmet_expectation{#condition " (at " __FILE__ ":" ALEX_STRINGIFY(__LINE__) ")"};            \
   }
-#endif
