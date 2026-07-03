d1ad0db7c65e68561e6c71e2d1788ce3e9a2c1cd
brobeson <brobeson@users.noreply.github.com>
2026-07-03 10:14:28 +0800

    Update errors.hpp

--- include/alexandria/errors.hpp
+++ include/alexandria/errors.hpp
@@ -8,16 +8,31 @@
 #include <stdexcept>
 
 namespace alex {
+  /// Thrown when a function precondition or class invariant fails.
   struct unmet_expectation: public std::logic_error {
+    /**
+     * \brief Construct an unmet_expection exception.
+     * \param[in] message An explanation of the failed precondition or invariant.
+     */
     explicit unmet_expectation(const char* const message):
       std::logic_error(message) {}
   };
 }
 
-#define ALEX_STRINGIFY_DETAIL(x) #x
-#define ALEX_STRINGIFY(x) ALEX_STRINGIFY_DETAIL(x)
+// Implementation details, equivalent to namespace alex::detail.
+#define ALEX_DETAIL_STRINGIFY_DETAIL(x) #x
+#define ALEX_DETAIL_STRINGIFY(x) ALEX_DETAIL_STRINGIFY_DETAIL(x)
 
-#define ALEX_EXPECTS( condition) \
+/**
+ * \brief Check a function precondition or class invariant.
+ * \param[in] condition A boolean expression representing the precondition or
+ * invariant. `false` indicates failure of the condition.
+ * \throws alex::unmet_expectation if \a condition is `false`
+ * \warning This macro assumes the condition is pass more often that fail. It
+ * uses `[[unlikely]]` for the failure case. You should profile your code to
+ * ensure this assumption doesn't deoptimize your code.
+ */
+#define ALEX_EXPECTS(condition) \
   if (!(condition)) [[unlikely]] {            \
-    throw alex::unmet_expectation{#condition " (at " __FILE__ ":" ALEX_STRINGIFY(__LINE__) ")"};            \
+    throw alex::unmet_expectation{#condition " (at " __FILE__ ":" ALEX_DETAIL_STRINGIFY(__LINE__) ")"};            \
   }
